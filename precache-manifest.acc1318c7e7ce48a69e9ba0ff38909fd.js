02db5423247fe70b1a52c796f81c625c07571745
luiz <luizhenriquefbb@gmail.com>
2019-07-08 10:56:21 -0300

    Updates

---
+++ precache-manifest.acc1318c7e7ce48a69e9ba0ff38909fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9c1e7cdf93ec74d8c2c68666bf0d97",
+    "url": "/evolutional-test-react/index.html"
+  },
+  {
+    "revision": "a41fe448c2dec5558ae3",
+    "url": "/evolutional-test-react/static/css/main.270f1166.chunk.css"
+  },
+  {
+    "revision": "708abd24644887891cb2",
+    "url": "/evolutional-test-react/static/js/2.2fc369bb.chunk.js"
+  },
+  {
+    "revision": "a41fe448c2dec5558ae3",
+    "url": "/evolutional-test-react/static/js/main.e98eda42.chunk.js"
+  },
+  {
+    "revision": "efd8dd9454f36f892785",
+    "url": "/evolutional-test-react/static/js/runtime~main.92858708.js"
+  }
+]);
