6fa9fbabd0f4b0caaf3bd4a2517db1309c8ff07f
luiz <luizhenriquefbb@gmail.com>
2019-07-09 13:53:00 -0300

    Updates

---
+++ precache-manifest.8f72065326072508550a24ab0e548bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7176dfe414d9c514e22ba13629b85422",
+    "url": "/evolutional-test-react/index.html"
+  },
+  {
+    "revision": "3e48ba0883c050d11ffc",
+    "url": "/evolutional-test-react/static/css/main.86164bd1.chunk.css"
+  },
+  {
+    "revision": "c2e504ea9e9296dde803",
+    "url": "/evolutional-test-react/static/js/2.dadafb40.chunk.js"
+  },
+  {
+    "revision": "3e48ba0883c050d11ffc",
+    "url": "/evolutional-test-react/static/js/main.372dcadd.chunk.js"
+  },
+  {
+    "revision": "efd8dd9454f36f892785",
+    "url": "/evolutional-test-react/static/js/runtime~main.92858708.js"
+  },
+  {
+    "revision": "37956f1ed57336f9c407eb58be82fef5",
+    "url": "/evolutional-test-react/static/media/in_development.37956f1e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/luizhenriquefbb/evolutional-test-react/precache-manifest.83a6cb93cb4c226112b95df09307ce9b.js"
+  "/evolutional-test-react/precache-manifest.8f72065326072508550a24ab0e548bcc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/luizhenriquefbb/evolutional-test-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/evolutional-test-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
